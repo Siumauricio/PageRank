b49bc4bda24f2fc84bc8b46b8a589ff213d7914c
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-11-03 21:12:30 -0600

    Update Archivo.cpp

--- Archivo.cpp
+++ Archivo.cpp
@@ -7,13 +7,23 @@
 #include "Nodo.h"
 #include "ListaEnlazada.h"
 using namespace std;
-
-void Archivo::LeerArchivo (string pagina,vector<string>lista,int contador,int contador2,ListaEnlazada &ls) {
+vector<string>lis;
+bool it = true;
+void Archivo::eiminarDuplicados(string pagina,int contador,ListaEnlazada &ls){
+	active = true;
+	activo = true;
+	cerrar = true;
+	cout << endl;
+	auto end = lis.end ();
+	for(auto it = lis.begin (); it != end; ++it) {
+		end = std::remove (it + 1,end,*it);
+	}
+	lis.erase (end,lis.end ());
 	string needle = "href=";
 	string cadena;
 	smatch match;
 	ifstream file (pagina);
-	if(contador < contador2) {  
+	if(contador < lis.size()) {
 		if(!file.is_open ()) {
 			cout << "Failed to open file!\n";
 			return;
@@ -29,11 +39,9 @@ void Archivo::LeerArchivo (string pagina,vector<string>lista,int contador,int co
 						if(active) {
 							ls.Anadir (pagina);
 							ls.Anadir (match[1].str ());
-							lista.push_back (match[1].str ());
 							active = false;
 						} else {
 							if(cerrar) {
-								lista.push_back (match[1].str ());
 								ls.Anadir (match[1].str ());
 							} else {
 								ls.Anadir (match[1].str ());
@@ -55,11 +63,72 @@ void Archivo::LeerArchivo (string pagina,vector<string>lista,int contador,int co
 	active = true;
 	activo = true;
 	cerrar = true;
-	if(contador2 == 1) {
+	if(lis.size()-1 == contador) {
 		return;
-	} else {
-		LeerArchivo (lista[contador],lista,contador + 1,contador2,ls);
+	} else{
+			contador+=1;
+			eiminarDuplicados (lis[contador],contador ,ls);
 	}
+}
+
+void Archivo::LeerArchivo (string pagina,vector<string>lista,int contador,int contador2) {
+	string needle = "href=";
+	string cadena;
+	smatch match;
+	bool crash = true;
+	ifstream file (pagina);
+	if(contador < contador2) {
+		if(!file.is_open ()) {
+			cout << "Failed to open file!\n";
+			return;
+		} else {
+			while(!file.eof ()) {
+				getline (file,cadena);
+				istringstream in (cadena);
+				if(cadena.find (needle) != std::string::npos) {
+					string line (cadena);
+					regex re ("href=\"(.*)\">");
+					if(regex_search (line,match,re)) {
+						activo = false;
+						if(active) {
+							lis.push_back (pagina);
+							lista.push_back (match[1].str ());
+							active = false;
+						} else {
+							if(cerrar) {
+								lista.push_back (match[1].str ());
+							}
+						}
+					}
+				}
+			}
+			cerrar = false;
+			if(activo) {
+				lis.push_back (pagina);
+			}
+		}
+	} else {
+		return ;
+	}
+	//ls.ImprimirLista ();
+	file.close ();
+	active = true;
+	activo = true;
+		cerrar = true;
+	if(contador2 == 1) {
+		return ;
+	} else {
+		LeerArchivo (lista[contador],lista,contador + 1,contador2);
+	}
+
+}
+bool Archivo::existePagina(string pagina){
+	for(int i = 0; i < lis.size(); i++) {
+		if(lis[i]==pagina){
+			return false;
+		}
+	}
+	return true;
 }
 int Archivo::cantidadHREF (string pagina) {
 	int contador = 0;
@@ -89,7 +158,7 @@ int Archivo::cantidadHREF (string pagina) {
 }
 vector<string> Archivo::extraerPaginas (string texto) {
 	string cadena;
-	vector<string>paginas = {"facebook.html","instagram.html","whatsapp.html","google.html","twitter.html","youtube.html","amazon.html"};
+	vector<string>paginas = {"instagram.html","whatsapp.html","facebook.html","google.html","youtube.html","amazon.html","twitter.html"};
 	vector<string>pages;
 	for(int i = 0; i < paginas.size (); i++) {
 		ifstream file (paginas[i]);
