75bb5923570551ab6eb114f85564e3a616a2d039
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-11-06 00:55:16 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,27 +2,29 @@
 #include "Archivo.h"
 #include "ListaEnlazada.h"
 #include <string>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 using namespace std;
 
-int main () {
+int main (int argc, char **argv) {
 	Archivo a;
 	vector<string>lista;
 	vector<string>lista3;
 	vector<string>ls2;
 	ListaEnlazada ls;
+	cout << "Palabra: " << argv[1] << endl;
 	NodoPagina *page2 = new NodoPagina ();
-	lista = a.extraerPaginas ("Photos");
+	lista = a.extraerPaginas (argv[1]);
 	for(int i = 0; i < lista.size (); i++) {
 		a.LeerArchivo (lista[i],lista3,0,a.cantidadHREF (lista[i]));
-		cout << lista[i] << endl;
+		//cout << lista[i] << endl;
 	}
 	a.eiminarDuplicados (lista[0],0,ls);
-     //ls.ImprimirLista ();
-	cout << endl << "-------------------------------------------" << endl;
 	ls.calcularRef (nullptr,ls2);
 	ls.AjustarPR (page2,ls2,0,0);
-	cout << endl << "-------------------------------------------" << endl;
-	ls.ImprimirPR ();
-	cout << "-------------------------------------------" << endl;
-
+	cout << "<h1>\t\tPAGERANK </h1>" << endl;
+	ls.ImprimirPR (argv[1]);
+	return 0;
 }
