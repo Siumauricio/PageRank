94f112b3a6189109ce7096ca11f1f107959300b6
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-11-13 16:20:31 -0600

    Update ListaEnlazada.h

--- ListaEnlazada.h
+++ ListaEnlazada.h
@@ -7,23 +7,24 @@ using namespace std;
 #define LISTAENLAZADA_H
 class ListaEnlazada {
 private:
+	array<double,20>valores;
 	NodoPagina*primero;
 	NodoRedirect* abajo;
 	string name;
+	bool exis = true;
 	bool guardar = true;
 	bool activo = true;
 	int cantHREF = 0;
 	int contador = 0;
 public:
+	ListaEnlazada (string);
 	ListaEnlazada(void);
 	bool estaVacia(void);
 	void Anadir(string);
-	double calcularPR (int,double,float);
-	void ImprimirLista();
+	double calcularPR (double,float);
 	vector<string> calcularRef (NodoPagina *,vector<string>&);
 	void ImprimirPR (string);
 	void AjustarPR (NodoPagina *page,vector<string>&,int,int);
 	void LimpiarPr ();
-
 };
 #endif // !NODO_H
